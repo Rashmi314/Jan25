f3378638a9b7894f9166b650d7f71c5b16da933b
Rashmi314 <46991593+Rashmi314@users.noreply.github.com>
2019-01-24 23:42:12 +0530

    Initial commit

--- src/qsp1/new1.java
+++ src/qsp1/new1.java
@@ -4,7 +4,10 @@ public class new1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a=10;
+		int b=10;
+		int sum=a+b;
+		System.out.println("sum");
 	}
 
 }
